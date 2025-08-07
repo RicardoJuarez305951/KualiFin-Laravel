43d85194f8e28443a3ec80bd8bd49945a9f0462c
CovIsaac <a278949@alumnos.uaslp.mx>
2025-08-06 21:53:53 -0600

    Change title

--- resources/views/components/layouts/authenticated.blade.php
+++ resources/views/components/layouts/authenticated.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title ?? config('app.name', 'Laravel') }}</title>
+    <title>{{ $title ?? config('app.name', 'Kaulifin') }}</title>
     @vite(['resources/css/app.css', 'resources/css/authenticated.css', 'resources/js/app.js'])
     
 </head>
