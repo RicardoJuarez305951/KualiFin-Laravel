5462de999062eac1dec7dcf23530a9cd8a220f7c
Ricardo <78766227+RicardoJuarez305951@users.noreply.github.com>
2025-09-05 16:51:15 -0600

    feat: add multi-pay store

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ import Alpine from 'alpinejs';
 import authenticated from './authenticated';
 import './stores/calc';
 import './stores/details';
+import './stores/multiPay';
 
 window.Alpine = Alpine;
 window.authenticated = authenticated;
