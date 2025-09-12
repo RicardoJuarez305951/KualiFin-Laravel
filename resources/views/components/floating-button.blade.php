b8b5e725a5b2fcdcd4b0889716404740ce22bcf2
Ricardo <78766227+RicardoJuarez305951@users.noreply.github.com>
2025-09-12 11:37:05 -0600

    feat: add floating shortcut button

---
+++ resources/views/components/floating-button.blade.php
@@ -0,0 +1,6 @@
+<a
+    href="{{ url('/consulta-base-datos-historica') }}"
+    class="fixed bottom-6 right-6 z-50 bg-blue-600 hover:bg-blue-700 text-white rounded-full p-4 shadow-lg"
+>
+    Consulta
+</a>
