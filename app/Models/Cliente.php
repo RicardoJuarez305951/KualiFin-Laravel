7d489d23a5edd071ec58d1e513df9ed11c0d4091
remptyg <87726198+remptyg@users.noreply.github.com>
2025-09-12 23:44:17 -0600

    Modelo que admite mas de un credito para venta

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -7,22 +8,50 @@ use Illuminate\Database\Eloquent\Model;
 class Cliente extends Model
 {
     use HasFactory;
-    protected $guarded = [];
-    const CREATED_AT = 'creado_en';
-    const UPDATED_AT = 'actualizado_en';
 
+    protected $table = 'clientes';
+    public $timestamps = false; 
+
+    protected $fillable = [
+        'promotor_id',
+        'CURP',
+        'nombre',
+        'apellido_p',
+        'apellido_m',
+        'fecha_nacimiento',
+        'tiene_credito_activo',
+        'estatus',
+        'monto_maximo',
+        'creado_en',
+        'actualizado_en',
+        'activo',
+    ];
+
+    protected $casts = [
+        'fecha_nacimiento' => 'date',
+        'tiene_credito_activo' => 'boolean',
+        'activo' => 'boolean',
+        'creado_en' => 'datetime',
+        'actualizado_en' => 'datetime',
+    ];
+    
     public function promotor()
     {
-        return $this->belongsTo(Promotor::class);
+        return $this->belongsTo(Promotor::class, 'promotor_id');
+    }
+
+    public function creditos()
+    {
+        return $this->hasMany(Credito::class, 'cliente_id');
     }
 
     public function credito()
     {
-        return $this->hasOne(Credito::class);
+        return $this->hasOne(Credito::class, 'cliente_id')->latestOfMany();
     }
 
     public function documentos()
     {
-        return $this->hasMany(DocumentoCliente::class);
+        return $this->hasMany(DocumentoCliente::class, 'cliente_id');
     }
 }
