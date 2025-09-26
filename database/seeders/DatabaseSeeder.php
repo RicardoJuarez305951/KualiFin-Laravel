e7cff03c678e82a3ad6157113413aa711646ca42
RicardoJuarez305951 <78766227+RicardoJuarez305951@users.noreply.github.com>
2025-09-26 02:43:03 -0600

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
             DemoCoreUserSeeder::class,
             DemoHierarchySeeder::class,
             DemoClientSeeder::class,
-            RealisticFilterCasesSeeder::class,
+            // RealisticFilterCasesSeeder::class,
         ]);
     }
 }
